4579ca647d2063a2d1989755b0debec8d151bbd3
makoto2025 <aratamakoto2025@gmail.com>
2025-10-29 18:23:15 -0700

    #13 save function

---
+++ data_management.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+class DataManagement:
+    def __init__(self, file_name="sampledata.csv"):
+        self.transactions = pd.DataFrame()
+        self.file_name = file_name
+
+    def select_menu(self):
+        print('\n' *2)
+        print('=== Personal Finance Tracker ===')
+        print('''
+ 0. Import Financial CSV Data
+ 1. View All Transactions
+ 2. View Transactions by Date Range
+ 3. Add a Transaction
+ 4. Edit a Transaction
+ 5. Delete a Transaction
+ 6. Analyze Spending by Category
+ 7. Calculate Average Monthly Spending
+ 8. Show Top Spending Category
+ 9. Set Monthly Income
+ 10. Set Category Budget
+ 11. Check Budget Status
+ 12. Visualize Spending Trends
+ 13. Save Transactions to CSV
+ 14. Exit
+                ''')
+        print('================================')
+        choice = input('Choose an option (0-14): ')
+        return choice
+    
+#0
+    def import_file(self):
+        file_path = input(f"Enter the path to the CSV file : ")
+        
+        if not file_path:
+            file_path = self.file_name
+        if file_path.endswith('.csv'):
+            self.transactions = pd.read_csv(file_path)
+            print("CSV file imported successfully.")
+            self.file_name = file_path  # update to last used name
+        else:
+            raise ValueError("Unsupported file format")
+
+#1
+    def view_all_transactions(self):
+        print("All Transactions:")
+        print(self.transactions)
+    
+#13
+    def save_transactions(self):
+        self.transactions.to_csv(self.file_name, index=False)
+        print(f"Transactions saved to {self.file_name}")
