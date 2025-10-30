8697e61d8155b6bba0e33081d08a56657b0499ba
Makoto <aratamakoto2025@gmail.com>
2025-10-29 18:11:24 -0700

    Makoto/issue14 (#23)
    * Create csv_import function, and app file to run
    
    * .DS_Store removed from tracking
    
    * Add a file for Data Management
    Create select menu
    
    * Set a select menu and conbine the import function
    
    * Remove unused space and break
    
    ---------
    
    Co-authored-by: ted-M-tech <tetsuya.maeda.mail@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from file_import import FileImporter
+from data_management import data_management
 
-file_importer_obj = FileImporter
-file_importer_obj.import_file()
+data_management_obj = data_management
 
-
-
+while True:
+    choice = data_management_obj.select_menu()
+    if choice == '0':
+        import_file = data_management_obj.import_file()
+    elif choice == '14':
+        print("Exiting the application.")
+        break
